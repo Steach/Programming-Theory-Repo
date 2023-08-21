b76f09408eaa97e414ffd2ee5617b6131e344386
Arthur Ustich <ta.ustich@gmail.com>
2023-08-21 22:12:55 +0300

    Turn Sys
    Added bounderies to rotate X.

--- Assets/Scripts/Turning.cs
+++ Assets/Scripts/Turning.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Turning : MonoBehaviour
 {
     [SerializeField] private float turnSpeed;
+    private Vector3 turnAngles;
+    private float xRotation = 40f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +17,25 @@ public class Turning : MonoBehaviour
     void Update()
     {
         Turn();
-        Debug.Log(transform.rotation.x);
     }
 
     void Turn()
     {
-        float inclineMovement = Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.left * inclineMovement);
-    }
+        float rotationX = transform.localEulerAngles.x;
+        
+        if (rotationX > 180)
+        {
+            rotationX -= 360;
+        }
 
-    void boundaryLimits()
-    {
+        float newRotationX = rotationX - Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
+        newRotationX = Mathf.Clamp(newRotationX, -xRotation, xRotation);
 
+        if (newRotationX < 0)
+        {
+            newRotationX += 360;
+        }
+
+        transform.localEulerAngles = new Vector3(newRotationX, transform.localEulerAngles.y, transform.localEulerAngles.z);
     }
 }
