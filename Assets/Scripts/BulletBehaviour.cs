17898f6eee9f453e80dcaedcc458d8be296970eb
Arthur Ustich <ta.ustich@gmail.com>
2023-09-03 20:52:58 +0300

    Modify shoot registration

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -6,83 +6,75 @@ public class BulletBehaviour : AssaultRifle
 {
     private float lifeTime;
     private Rigidbody bulletrb;
+    private Vector3 currentPosition;
     private Vector3 previuslyPosition;
     private GameObject target = null;
     // Start is called before the first frame update
     void Start()
     {
+        previuslyPosition = transform.position;
         lifeTime = 0;
-        bulletSpeed = 0.5f;
+        bulletSpeed = 25f;
         bulletrb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 bulletDirection = -transform.right;
-        lifeTime += Time.deltaTime;
-        //transform.Translate(Vector3.left * bulletSpeed * Time.deltaTime);
-        bulletrb.AddForce(bulletDirection * bulletSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
-        //Debug.DrawLine(transform.position, (transform.position + bulletDirection) * 100, Color.red);
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, bulletDirection, out hit, 1000))
-        {
-            if (hit.collider != null & hit.collider.CompareTag("TargetBody"))
-            {
-                if (target == null)
-                {
-                    target = hit.collider.gameObject;
-                }
-                Debug.Log("Bullet see target");
-            }
-            
-            else if (target != null)
-            {
-                Debug.Log("Bullet hit the target!");
-                Destroy(gameObject);
-            }
-        }
-        
-        previuslyPosition = transform.position;
-        //BulletDeath();
+        VarificationShoot();
+        BulletDeath();
     }
 
     void BulletDeath()
     {
-        if(lifeTime >= 3)
+        if(lifeTime >= 7)
         {
             Destroy(gameObject);
         }
     }
 
-    /*private void OnTriggerEnter(Collider other)
+
+    void VarificationShoot()
     {
-        if(other.CompareTag("TargetHead"))
-        {
-            Debug.Log("HeadShot");
-            Destroy(gameObject);
-        }
+        currentPosition = transform.position;
 
-        if(other.CompareTag("TargetBody"))
-        {
-            Debug.Log("BodyShot");
-            Destroy(gameObject);
-        }
-    }*/
+        Vector3 bulletDirection = -transform.right;
+        Vector3 direction = currentPosition - previuslyPosition;
+        float distence = direction.magnitude;
+        bulletrb.AddForce(bulletDirection * bulletSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        lifeTime += Time.deltaTime;
 
-    /*private void OnCollisionEnter(Collision other) 
-    {
-        if(other.gameObject.CompareTag("TargetHead"))
+        Debug.DrawLine(currentPosition, previuslyPosition, Color.red);
+        RaycastHit hit;
+        if (Physics.Raycast(currentPosition, direction, out hit, distence))
         {
-            Debug.Log("HeadShot");
-            Destroy(gameObject);
+            if (hit.collider.CompareTag("TargetBody"))
+            {
+                
+                Debug.Log("Bullet hit the target");
+            }
+            
+            
         }
-
-        if(other.gameObject.CompareTag("TargetBody"))
-        {
-            Debug.Log("BodyShot");
-            Destroy(gameObject);
-        }
-    }*/
+        
+        previuslyPosition = currentPosition;
+    }
 }
+
+/*
+draft
+
+if (target == null)
+{
+    target = hit.collider.gameObject;
+}
+else if (target != null)
+{
+    Debug.Log("Bullet hit the target!");
+    Destroy(gameObject);
+}
+
+//transform.Translate(Vector3.left * bulletSpeed * Time.deltaTime);
+//Debug.DrawLine(transform.position, (transform.position + bulletDirection) * 100, Color.red);
+
+*/
